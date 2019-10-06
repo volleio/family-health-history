573d78faa923535015319a2c2ff8cfaa5153e19b
Lucas Volle <Lucas@Volle.io>
2019-10-06 01:10:26 -0400

    More styling changes and functionality for adding nodes and editing medical conditions

--- src/client/main.ts
+++ src/client/main.ts
@@ -1,4 +1,5 @@
 declare var OrgChart: any;
+declare var Def: any;
 
 class FamilyHealthHistoryClient {
 	private mainContainer: HTMLElement;
@@ -30,6 +31,16 @@ class FamilyHealthHistoryClient {
 
 	private Initialize()
 	{
+		// window.addEventListener("mousemove", (evt) => {
+		// 	const logoImg = document.querySelector(".logo") as HTMLElement;
+		// 	const logoImgRect = logoImg.getBoundingClientRect();
+		// 	const centerX = logoImgRect.left + (logoImgRect.right - logoImgRect.left) * 0.5;
+		// 	const centerY = logoImgRect.top + (logoImgRect.bottom - logoImgRect.top) * 0.5;
+		// 	const x = (centerX - evt.clientX) / 10;
+		// 	const y = (centerY - evt.clientY) / 20  * -1;
+		// 	logoImg.style.transform = `translate(-50%) rotate3d(1, 0, 0, ${y}deg) rotate3d(0, 1, 0, ${x}deg)`;
+		// });
+
 		this.loginInput.addEventListener('keydown', (evt) => 
 		{
 			requestAnimationFrame(() => 
@@ -282,6 +293,14 @@ class FamilyHealthHistoryClient {
 		this.loginButton.style.display = 'none';
 		this.loginContainer.classList.add('login-container--logged-in');
 		this.loginInput.classList.add('login-input--logged-in');
+		document.querySelector(".logo.hands").style.opacity = "1";
+
+		window.setTimeout(() =>
+		{
+			document.querySelector(".logo.tree").classList.add("logged-in");
+			document.querySelector(".logo.hands").classList.add("logged-in");
+			document.querySelector(".logo.hands").style.opacity = "";
+	}, 1000);
 		
 		this.loginAuthBadge.style.display = 'block';
 		this.loginAuthBadgeCheck.classList.add('animate-in');
@@ -333,6 +352,12 @@ class FamilyHealthHistoryClient {
 			scaleInitial: 0.7,
 			orientation: OrgChart.orientation.bottom,
 			layout: OrgChart.tree,
+			toolbar: {
+                layout: true,
+                zoom: true,
+                fit: true,
+                expandAll: false
+            },
 			nodeMouseClick: OrgChart.action.edit,
 			menu: {
 				svg: { text: "Export SVG" },
@@ -340,13 +365,35 @@ class FamilyHealthHistoryClient {
 			},
 			nodeMenu:{
             	add: {text:"Add Parent"},
-            	addSibling: {text:"Add Sibling"},
-            	addChild: {text:"Add Child"},
+            	addSibling: {
+					text:"Add Sibling",
+					icon: () => '<svg width="24px" height="24px"   viewBox="0 0 922 922"><path fill="#7A7A7A" d="M922,453V81c0-11.046-8.954-20-20-20H410c-11.045,0-20,8.954-20,20v149h318c24.812,0,45,20.187,45,45v198h149 C913.046,473.001,922,464.046,922,453z" /><path fill="#7A7A7A" d="M557,667.001h151c11.046,0,20-8.954,20-20v-174v-198c0-11.046-8.954-20-20-20H390H216c-11.045,0-20,8.954-20,20v149h194 h122c24.812,0,45,20.187,45,45v4V667.001z" /><path fill="#7A7A7A" d="M0,469v372c0,11.046,8.955,20,20,20h492c11.046,0,20-8.954,20-20V692v-12.501V667V473v-4c0-11.046-8.954-20-20-20H390H196 h-12.5H171H20C8.955,449,0,457.955,0,469z" /></svg>',
+					onClick: (nodeId) =>
+					{
+						const node = this.familyTree.get(nodeId);
+						const nextId = this.GetNextNodeId();
+						this.familyTree.addNode({ id: nextId, pid: node.id, tags: ["assistant"] });
+					}
+				},
+            	addChild: {
+					text:"Add Child",
+					icon: () => '<svg width="24px" height="24px"   viewBox="0 0 922 922"><path fill="#7A7A7A" d="M922,453V81c0-11.046-8.954-20-20-20H410c-11.045,0-20,8.954-20,20v149h318c24.812,0,45,20.187,45,45v198h149 C913.046,473.001,922,464.046,922,453z" /><path fill="#7A7A7A" d="M557,667.001h151c11.046,0,20-8.954,20-20v-174v-198c0-11.046-8.954-20-20-20H390H216c-11.045,0-20,8.954-20,20v149h194 h122c24.812,0,45,20.187,45,45v4V667.001z" /><path fill="#7A7A7A" d="M0,469v372c0,11.046,8.955,20,20,20h492c11.046,0,20-8.954,20-20V692v-12.501V667V473v-4c0-11.046-8.954-20-20-20H390H196 h-12.5H171H20C8.955,449,0,457.955,0,469z" /></svg>',
+					onClick: (nodeId) =>
+					{
+						const node = this.familyTree.get(nodeId);
+						const nextId = this.GetNextNodeId();
+
+						
+
+						this.familyTree.addNode({ id: nextId, pid: node.id, tags: ["assistant"] });
+					}
+				},
             	remove: {text:"Remove"}
             },
 			nodeBinding: {
 				field_0: "Name",
 				field_1: "Relation",
+				field_2: "Medical Conditions",
 				img_0: "img"
 			},
 			tags: {
@@ -357,20 +404,115 @@ class FamilyHealthHistoryClient {
 				f5: familyGroupTag
 			},
 			nodes: [
-				{ id: 9, pid: 3, Name: "Leonard Smith", Relation: "Paternal Grandfather", img: "https://vignette.wikia.nocookie.net/rickandmorty/images/7/74/Leonard_Smith.png/revision/latest?cb=20140421044137" },
-				{ id: 8, pid: 3, Name: "Joyce Smith", Relation: "Paternal Grandmother", img: "https://rickandmortyapi.com/api/character/avatar/186.jpeg" },
-				{ id: 7, pid: 4, Name: "Rick Sanchez", Relation: "Maternal Grandfather", img: "https://vignette.wikia.nocookie.net/rickandmorty/images/a/a6/Rick_Sanchez.png/revision/latest?cb=20160923150728" },
-				{ id: 6, pid: 4, Name: "Mrs. Sanchez", Relation: "Maternal Grandmother", img: "https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_960_720.png" },
-				{ id: 5, pid: 3, tags: ["assistant"], Name: "Steve Smith", Relation: "\"Uncle\"", img: "https://vignette.wikia.nocookie.net/rickandmorty/images/5/50/UncleSteve.png/revision/latest?cb=20150823180126" },
-				{ id: 4, pid: 1, Name: "Beth Smith", Relation: "Mom", img: "https://vignette.wikia.nocookie.net/rickandmorty/images/b/be/Screenshot_2016-11-20_at_6.54.33_PM.png/revision/latest?cb=20161121033552" },
-				{ id: 3, pid: 1, Name: "Jerry Smith", Relation: "Dad", img: "https://pbs.twimg.com/profile_images/738078769920020481/xpW4r-Tr_400x400.jpg" },
-				{ id: 2, pid: 1, tags: ["assistant"], Name: "Summer Smith", Relation: "Sister", img: "https://vignette.wikia.nocookie.net/rickandmorty/images/a/ad/Summer_is_cool.jpeg/revision/latest?cb=20160919183158" },
-				{ id: 1, Name: "Morty Smith", Relation: "You", img: "https://media3.giphy.com/media/EE8AyDbOjlfOw/source.gif" },
+				{ id: 9, pid: 3, Name: "Leonard Smith", Relation: "Paternal Grandfather", img: "https://vignette.wikia.nocookie.net/rickandmorty/images/7/74/Leonard_Smith.png/revision/latest?cb=20140421044137", "Medical Conditions": "" },
+				{ id: 8, pid: 3, Name: "Joyce Smith", Relation: "Paternal Grandmother", img: "https://rickandmortyapi.com/api/character/avatar/186.jpeg", "Medical Conditions": "" },
+				{ id: 7, pid: 4, Name: "Rick Sanchez", Relation: "Maternal Grandfather", img: "https://vignette.wikia.nocookie.net/rickandmorty/images/a/a6/Rick_Sanchez.png/revision/latest?cb=20160923150728", "Medical Conditions": "" },
+				{ id: 6, pid: 4, Name: "Mrs. Sanchez", Relation: "Maternal Grandmother", img: "https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_960_720.png", "Medical Conditions": "" },
+				{ id: 5, pid: 3, tags: ["assistant"], Name: "Steve Smith", Relation: "\"Uncle\"", img: "https://vignette.wikia.nocookie.net/rickandmorty/images/5/50/UncleSteve.png/revision/latest?cb=20150823180126", "Medical Conditions": "" },
+				{ id: 4, pid: 1, Name: "Beth Smith", Relation: "Mom", img: "https://vignette.wikia.nocookie.net/rickandmorty/images/b/be/Screenshot_2016-11-20_at_6.54.33_PM.png/revision/latest?cb=20161121033552", "Medical Conditions": "" },
+				{ id: 3, pid: 1, Name: "Jerry Smith", Relation: "Dad", img: "https://pbs.twimg.com/profile_images/738078769920020481/xpW4r-Tr_400x400.jpg", "Medical Conditions": "" },
+				{ id: 2, pid: 1, tags: ["assistant"], Name: "Summer Smith", Relation: "Sister", img: "https://vignette.wikia.nocookie.net/rickandmorty/images/a/ad/Summer_is_cool.jpeg/revision/latest?cb=20160919183158", "Medical Conditions": "" },
+				{ id: 1, Name: "Morty Smith", Relation: "You", img: "https://i.imgur.com/9NGko96.gif", "Medical Conditions": "" },
 			]
 		});
 
+		this.familyTree.on('click', (sender, node) =>
+		{
+			window.setTimeout(() => 
+			{
+				console.log(sender);
+				console.log(node);
+
+				const medicalConditionsSection = sender.editUI.wrapperElement.querySelector('[label="Medical Conditions"]').parentElement;
+				const medicalConditionsInput = medicalConditionsSection.querySelector("input");
+				medicalConditionsSection.querySelector("hr").style.display = "none";
+				medicalConditionsInput.style.display = "none";
+
+				const medicalConditionSearch = document.createElement("input") as any;
+				medicalConditionSearch.type	= "text";
+				medicalConditionSearch.id = "condition";
+				medicalConditionSearch.className = "condition-search";
+				medicalConditionsSection.appendChild(medicalConditionSearch);
+				
+				const medicalConditionsContainer = document.createElement("div");
+				medicalConditionsSection.appendChild(medicalConditionsContainer);
+
+				if (!node["Medical Conditions"] || node["Medical Conditions"] === "")
+						node["Medical Conditions"] = "[]";
+
+				const medicalConditions = JSON.parse(node["Medical Conditions"]) as IMedicalCondition[];
+
+				this.UpdateMedicalConditionsUI(medicalConditions, medicalConditionsContainer, (index) =>
+				{
+					medicalConditions.splice(index, 1);
+					node["Medical Conditions"] = JSON.stringify(medicalConditions);
+					medicalConditionsInput.value = node["Medical Conditions"];
+				});
+
+				new Def.Autocompleter.Search('condition', 'https://clinicaltables.nlm.nih.gov/api/conditions/v3/search?ef=consumer_name,icd10cm,info_link_data');
+
+				Def.Autocompleter.Event.observeListSelections('condition', () => {
+					if (!medicalConditionSearch.autocomp || !medicalConditionSearch.autocomp.getSelectedItemData() || 
+						!medicalConditionSearch.autocomp.getSelectedItemData()[0] || !medicalConditionSearch.autocomp.getSelectedItemData()[0].data)
+						return;
+
+					const info = medicalConditionSearch.autocomp.getSelectedItemData()[0].data;
+
+					medicalConditions.push({
+						name: info.consumer_name,
+						icd10Code: info.icd10cm[0].code,
+						icd10Text: info.icd10cm[0].text,
+						link: info.info_link_data[0][0],
+						linkText: info.info_link_data[0][1],
+					});
+
+					node["Medical Conditions"] = JSON.stringify(medicalConditions);
+					medicalConditionsInput.value = node["Medical Conditions"];
+
+					this.UpdateMedicalConditionsUI(medicalConditions, medicalConditionsContainer, (index) =>
+					{
+						medicalConditions.splice(index, 1);
+						node["Medical Conditions"] = JSON.stringify(medicalConditions);
+						medicalConditionsInput.value = node["Medical Conditions"];
+					});
+				});
+			}, 0);
+		});
+
+		window.setTimeout(() =>	document.getElementById("tree").style.opacity = "1", 2000);
 	}
 
+	private UpdateMedicalConditionsUI(medicalConditions: IMedicalCondition[], medicalConditionsContainer: HTMLElement, onDelete: (index: number) => void)
+	{
+		medicalConditionsContainer.innerHTML = "";
+		medicalConditions.forEach((obj, index) => {
+			const conditionContainer = document.createElement("div");
+			conditionContainer.className = "condition-container";
+			conditionContainer.innerHTML = `
+			<strong>${obj.name}</strong><i>${obj.icd10Code}: ${obj.icd10Text}</i>
+			<br/>
+			<a href="${obj.link}">nlm.nih.gov: ${obj.linkText}</a><div class="delete-btn">×</div>
+			`
+			medicalConditionsContainer.appendChild(conditionContainer);
+			conditionContainer.querySelector(".delete-btn").addEventListener("click", () => {
+				conditionContainer.remove();
+				onDelete(index);
+			});
+		});
+	}
+
+	private GetNextNodeId(): number
+	{
+		return Math.max.apply(Math, this.familyTree.config.nodes.map((node) => node.id)) + 1;
+	}
+}
+
+interface IMedicalCondition {
+	name: string;
+	icd10Code: string,
+	icd10Text: string,
+	link: string,
+	linkText: string,
 }
 
 enum HelpStates {
